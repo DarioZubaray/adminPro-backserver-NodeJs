fe119ab4dc44fb2269581918b132ee41f7b2dc97
dario.zubaray <dzubaray@imagosur.com>
2019-08-29 09:05:14 -0300

    creando la primera ruta de express

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var express = require('express');
 // Inicializar variables
 var app = express();
 
+// rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Primera petision realizada correctamente'
+    });
+});
+
 // Escuchar peticiones
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto: \x1b[32m%s\x1b[0m', '3000')
