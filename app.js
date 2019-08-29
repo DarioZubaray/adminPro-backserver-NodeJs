f57e514e33dff7f85eba21dd75d1834b67f1aa05
dario.zubaray <dzubaray@imagosur.com>
2019-08-29 09:37:05 -0300

    conectando node con mongo a traves de mongoose

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // Require
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
+
+// Conexion a la db
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if(err) throw err;
+    console.log('Conexion a la base de datos en el puerto: \x1b[32m%s\x1b[0m con exito', '27017')
+});
 
 // rutas
 app.get('/', (req, res, next) => {
     res.status(200).json({
         ok: true,
-        mensaje: 'Primera petision realizada correctamente'
+        mensaje: 'Primera peticion realizada correctamente'
     });
 });
 
