025676aad47c61a0b667da34d7d7c37ca7077520
dario.zubaray <dzubaray@imagosur.com>
2019-09-04 08:39:31 -0300

    agregando la libreria de google-auth y obteniendo los datos del usuario logueado a traves del token

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@este-es@un-seed-dificil';
+
+// module.exports.CLIENT_ID = '1049010283951-jljgd6tpb6ejoqi46aetu3tnnk095lej.apps.googleusercontent.com'
+module.exports.CLIENT_ID = '1049010283951-ajtdkpvleub4nfd04rvbo1bcl7rlgiig.apps.googleusercontent.com'
--- routes/login.js
+++ routes/login.js
@@ -8,13 +8,48 @@ var app = express();
 
 var Usuario = require('../models/usuario');
 
+const {OAuth2Client} = require('google-auth-library');
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const client = new OAuth2Client(CLIENT_ID);
+
 // ==================================================
 // Login google
 // ==================================================
-app.post('/google', (req, res, next) => {
-    res.status(200).json({
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    console.log(ticket);
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    console.log(payload);
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async (req, res, next) => {
+    var token = req.body.token;
+
+    var googleUser = await verify(token).catch( e => {
+        return res.status(403).json({
+            ok: false,
+            mensaje: 'Token no válido'
+        });
+    });
+
+    return res.status(200).json({
         ok: true,
-        mensaje: 'Primera peticion realizada correctamente'
+        mensaje: 'Primera peticion realizada correctamente',
+        googleUser: googleUser
     });
 });
 
